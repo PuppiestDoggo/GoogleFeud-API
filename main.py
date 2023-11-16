4356ca00b3dc89435af02bf9f95006ea8ab3bd03
blackmine57 <blackmine57@protonmail.com>
2023-11-16 18:04:21 +0100

    v0.0.0.0.0.0.1

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
-import requests, json
-app = FastAPI()
+import json
+import random
 
+import requests
+from fastapi import FastAPI
+
+app = FastAPI()
+queries = ["Are dogs ","Are puppies "]
 headers = {
     "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 "
     "Safari/537.36 Edge/18.19582"
 }
 
+@app.get("/getquery/")
+async def get_query():
+    return random.choice(queries)
 
 @app.get("/query/{query}")
 async def query(query: str):
